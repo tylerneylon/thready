a728996bf814b2b24894fb18c2100e781d181ed5
Tyler Neylon <tylerneylon@gmail.com>
2014-11-18 13:00:16 -0800

    Update the comments in thready.h.

--- thready/thready.h
+++ thready/thready.h
@@ -7,22 +7,7 @@
 // ensure safety; this is sometimes referred to as the actor model:
 // http://en.wikipedia.org/wiki/Actor_model
 //
-
-// TODO Update all comments.
-
-// Design:
-//
-// A thready message is a json_Item.
-//
-// Ownership of json_Items is given to thready when they are sent
-// and then to the receiver when received.
-//
-// Non-main threads are automatically started in an event loop, and operate
-// by means of a given callback.
-//
-// The main thread may either be send-only, or, in order to receive messages,
-// must regularly call thready__runloop(callback) which will provide any
-// received messages.
+// See the readme for an introduction to using this library.
 //
 
 #pragma once
